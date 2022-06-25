cf4b2565694d7b4058bba2238d1d5d2d60b17ff7
Alexey Rytsku <topelven1234567890@gmail.com>
2022-06-25 16:32:49 +0300

    Adding pages

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from firstapp import views
 
 urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
